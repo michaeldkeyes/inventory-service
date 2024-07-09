699df131762bde4229ab49db3b7743057a0b1dc2
Micky <micky@Marias-iMac.local>
2024-07-09 13:25:05 -0700

    tests

--- src/test/java/com/example/inventory/InventoryApplicationTests.java
+++ src/test/java/com/example/inventory/InventoryApplicationTests.java
@@ -1,15 +1,65 @@
 package com.example.inventory;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeAll
+    static void setUpAll() {
+		mySQLContainer.start();
+	}
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	@AfterAll
+	static void tearDownAll() {
+		mySQLContainer.stop();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldReadInventory() {
+		final boolean response = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=iphone_15&quantity=1")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+
+		assertTrue(response);
+
+		final boolean negativeResponse = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=iphone_15&quantity=1000")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+
+		assertFalse(negativeResponse);
 	}
 
 }
